5b95a0e1a34fdc0422a4eff31049d666b9e157e5
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-01-20 16:17:30 +0100

    Logging default is debug

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ var redis = require("redis");
 var createApp = require("./lib/createApp");
 
 var log = bunyan.createLogger({name: "coordinator"});
-var redisClient = redis.createClient(config.redis.port, config.redis.host);
+var env = process.env.NODE_ENV || "development";
 
+if (env == "development") {
+    log.level("debug");
+}
+var redisClient = redis.createClient(config.redis.port, config.redis.host);
 redisClient.on("error", function(err) {
     log.error("Redis error", err);
 });
 
 var app = createApp(log, redisClient);
 
+log.info("Waiting for redis server " + config.redis.host + ":" + config.redis.port + " to start");
 redisClient.on("ready", function() {
     log.info("Redis is ready at http://%s:%s", config.redis.host, config.redis.port);
     var server = app.listen(config.port, function () {
