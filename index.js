3a7560bf2bf8cd60dd51d99eb62e35845fadcff4
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-01-25 15:05:51 +0100

    Add more info

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ if (env == "development") {
     log.level("debug");
 }
 var redisClient;
-
+var redisLocation;
 if (config.redis.socket) {
     redisClient = redis.createClient(config.redis.socket);
+    redisLocation = config.redis.socket;
 } else {
+    redisLocation = config.redis.host + ":" + config.redis.port;
     redisClient = redis.createClient(config.redis.port, config.redis.host);
 }
 redisClient.on("error", function(err) {
@@ -24,9 +26,9 @@ redisClient.on("error", function(err) {
 
 var app = createApp(log, redisClient);
 
-log.info("Waiting for redis server " + config.redis.host + ":" + config.redis.port + " to start");
+log.info("Waiting for connection to redis server " + redisLocation);
 redisClient.on("ready", function() {
-    log.info("Redis is ready at http://%s:%s", config.redis.host, config.redis.port);
+    log.info("Redis is ready at " + redisLocation);
     var server = app.listen(config.port, function () {
         var host = server.address().address
         var port = server.address().port
